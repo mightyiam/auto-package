40b17ceca111714bfee05ae6207c3049e91c975e
jmanero <john.manero@gmail.com>
2015-02-03 00:42:51 -0500

    Starting project

---
+++ package.js
@@ -0,0 +1,9 @@
+Package = require('./index');
+
+Package.name('auto-package');
+Package.version_file();
+Package.author('John Manero', 'john.manero@gmail.com');
+Package.description('Dynamically generate package.json files for NodeJS modules');
+Package.github_repo('jmanero/auto-package');
+Package.keyword('package.json');
+Package.license('MIT');
